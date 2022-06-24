7e43c88a401fd650c3db05ecef5d2b60cb3c8fa1
chaeng <cathyshim222@naver.com>
2022-06-24 23:03:52 +0900

    home에 다이어리 목록 띄우기 useContext()사용

--- src/App.js
+++ src/App.js
@@ -36,9 +36,38 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext()
 export const DiaryDispatchContext = React.createContext()
 
+const dummyData = [
+  {
+    id:1,
+    emotion:1,
+    content: "오늘의일기 1번",
+    date: 1656077942134
+  }, {
+    id:2,
+    emotion:2,
+    content: "오늘의일기 2번",
+    date: 1656077942136
+  }, {
+    id:3,
+    emotion:3,
+    content: "오늘의일기 3번",
+    date: 1656067866317
+  }, {
+    id:4,
+    emotion:4,
+    content: "오늘의일기 4번",
+    date: 1656067866318
+  }, {
+    id:5,
+    emotion:5,
+    content: "오늘의일기 5번",
+    date: 1656067866319
+  },
+]
+
 function App() {
   //REDUCER
-  const [data, dispatch] = useReducer(reducer,[])
+  const [data, dispatch] = useReducer(reducer,dummyData)
   
   const dataId = useRef(0) //일기의 고유 id 생성
   //CREATE
