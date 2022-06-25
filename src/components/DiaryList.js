864ee7e261185e2df98980185e398f0e7bd60d3a
chaeng <cathyshim222@naver.com>
2022-06-25 23:38:29 +0900

    메뉴바 css

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -18,7 +18,7 @@ const filterOptionList = [
 //일기 정렬 셀렉트 태그 생성 : 날짜순, 감정순
 const ControlMenu = ({value, onChange, optionList}) => {
     return (
-        <select value={value} onChange={(e)=>onChange(e.target.value)}>
+        <select className='ControlMenu' value={value} onChange={(e)=>onChange(e.target.value)}>
             {optionList.map((it,idx)=><option key={idx} value={it.value}>{it.name}</option>)}
         </select>
     )
@@ -54,10 +54,16 @@ const DiaryList = ({diaryList}) => {
     }
 
     return (
-        <div>
-            <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
-            <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList}/>
-            <MyButton type={'positive'} text={'새 일기쓰기'} onClick={()=>navigate('/new')}/>
+        <div className='DiaryList'>
+            <div className='menu_wrapper'>
+                <div className='left_col'>
+                    <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
+                    <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList}/>
+                </div>
+                <div className='right_col'>
+                    <MyButton type={'positive'} text={'새 일기쓰기'} onClick={()=>navigate('/new')}/>
+                </div>
+            </div>
             {getProcessedDiaryList().map((it)=>(
                 <div key={it.id}>
                     {it.content} {it.emotion}
